5a63a83635cc728a974d5e79e9e1a4024f5cef55
izng-code <129785832+izng-code@users.noreply.github.com>
2023-07-12 10:18:52 +0200

    Refactor code and add pixel resize functionality

--- sketch.js
+++ sketch.js
@@ -1,22 +1,40 @@
-let container = document.querySelector("body");
-let containerDiv = [];
-let pixelDiv = [];
-let counter = 0;
-for (let i = 0; i < 16; i++) {
-    containerDiv.push(document.createElement("div"));
-    containerDiv[i].classList.add("container");
-    containerDiv[i].id = `container_${i}`;
-    container.append(containerDiv[i]);
-    for (let j = 0; j < 16; j++) {
-        pixelDiv.push(document.createElement("div"));
-        pixelDiv[counter].classList.add("pixel");
-        document.querySelector(`#container_${i}`).append(pixelDiv[counter]);
-        counter++
+function buildGrid(size) {
+    let container = document.querySelector(".box");
+    let containerDiv = [];
+    let pixelDiv = [];
+    let counter = 0;
+    let calculatedDim = (640 / size).toString() + "px"
+    for (let i = 0; i < size; i++) {
+        containerDiv.push(document.createElement("div"));
+        containerDiv[i].classList.add("container");
+        containerDiv[i].id = `container_${i}`;
+        container.append(containerDiv[i]);
+        for (let j = 0; j < size; j++) {
+            pixelDiv.push(document.createElement("div"));
+            pixelDiv[counter].classList.add("pixel");
+            pixelDiv[counter].style.width = calculatedDim;
+            pixelDiv[counter].style.height = calculatedDim;
+            document.querySelector(`#container_${i}`).append(pixelDiv[counter]);
+            counter++
+        }
     }
-}
-let pixels = document.querySelectorAll(".pixel");
-pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseout", () => {
+    let pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseout", () => {
         pixel.classList.add("active");
     });
+});
+
+};
+buildGrid(16);
+document.getElementById("button").addEventListener("click", () => {
+    let gridSize = prompt("Enter a new grid size.")
+    gridSize = parseInt(gridSize);
+    if (gridSize % 1 != 0) {
+        alert("Invalid! Must be whole number!");
+    }
+    else {
+        document.querySelector(".box").innerHTML = "";
+        buildGrid(gridSize);
+    }
 });
